c02a747deaa9d31898533c2bd8eacf3afaa6fd90
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-02 17:09:35 +0000

    Create landing page hero section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
+import Hero from "../components/landingPage/hero/Hero";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
       <Footer />
     </div>
   );
