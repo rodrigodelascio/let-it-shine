062b1c9a1f157c87f9a8c8679662958e952d1bd8
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-03 15:48:52 +0000

    Add extra styles

--- src/components/landingPage/hero/LandingHero.js
+++ src/components/landingPage/hero/LandingHero.js
@@ -11,9 +11,9 @@ const LandingHero = () => {
         <h1>Professional cleaning service</h1>
         <h2>For your workplace or home</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <div>
-          <button>BUTTON 1</button>
-          <button>BUTTON 2</button>
+        <div className={styles["hero-buttons-wrapper"]}>
+          <button className={styles["hero-button-a"]}>BUTTON 1</button>
+          <button className={styles["hero-button-b"]}>BUTTON 2</button>
         </div>
       </section>
       <section className={styles["hero-socials"]}>
