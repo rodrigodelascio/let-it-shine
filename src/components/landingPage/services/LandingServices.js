f3e9087647d5cc4d20de2afc413ea6245ee19def
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-05 11:26:13 +0000

    Add further styles

---
+++ src/components/landingPage/services/LandingServices.js
@@ -0,0 +1,28 @@
+import styles from "./LandingServices.module.css";
+
+const LandingServices = () => {
+  return (
+    <div className={styles["services-wrapper"]}>
+      <section className={styles["services-container"]}>
+        <h1>Services</h1>
+        <h2>For your workplace or home</h2>
+        <p>
+          Elevate your environment with our expert touch, because your space
+          deserves to shine as bright as you do. Discover a new standard of
+          clean with us. Let It Shine!
+        </p>
+      </section>
+      <section>
+        <h1>Services</h1>
+        <h2>For your workplace or home</h2>
+        <p>
+          Elevate your environment with our expert touch, because your space
+          deserves to shine as bright as you do. Discover a new standard of
+          clean with us. Let It Shine!
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default LandingServices;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,14 @@
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
 import LandingHero from "../components/landingPage/hero/LandingHero";
+import LandingServices from "../components/landingPage/services/LandingServices";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
       <LandingHero />
+      <LandingServices />
       <Footer />
     </div>
   );
