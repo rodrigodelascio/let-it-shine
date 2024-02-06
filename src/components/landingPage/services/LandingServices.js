6d24b9112dd39e063a4a024290635a068ede8372
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-06 17:37:00 +0000

    Add styles + Update cards

--- src/components/landingPage/services/LandingServices.js
+++ src/components/landingPage/services/LandingServices.js
@@ -1,34 +1,47 @@
 import styles from "./LandingServices.module.css";
 import ServiceCard from "./ServiceCard";
+import { BsDashLg } from "react-icons/bs";
+import cleaningProducts from "../../../assets/images/wepik-export-20240206155722Dzmx.png";
 
 const LandingServices = () => {
   return (
     <div className={styles["services-wrapper"]}>
       <section className={styles["services-container"]}>
-        <h1>Services</h1>
-        <h2>For your workplace or home</h2>
+        <div className={styles["services-header-container"]}>
+          <BsDashLg className={styles["dash-icon"]} />
+          <h1>Services</h1>
+        </div>
+        <h2>
+          What we do for <span>our clients</span>
+        </h2>
         <p>
-          Elevate your environment with our expert touch, because your space
-          deserves to shine as bright as you do. Discover a new standard of
-          clean with us. Let It Shine!
+          We're experts in delivering meticulous cleaning solutions tailored to
+          your needs. Our dedicated team ensures every corner of your home or
+          office dazzles with cleanliness.
         </p>
+        <div className={styles["cleaning-products-container"]}>
+          <img
+            src={cleaningProducts}
+            alt="cleaning products"
+            className={styles["cleaning-products"]}
+          />
+        </div>
       </section>
       <section className={styles["services-cards-container"]}>
         <ServiceCard
           cardNumber={"1"}
           serviceTitle={"Office Cleaning"}
-          serviceDescription={"Lorem ipsum"}
-          className={styles["blue-number"]}
+          className={styles["green-number"]}
         />
         <ServiceCard
           cardNumber={"2"}
           serviceTitle={"Home Cleaning"}
-          serviceDescription={"Lorem ipsum"}
+          className={styles["blue-number"]}
         />
         <ServiceCard
           cardNumber={"3"}
           serviceTitle={"Deep Cleaning"}
-          serviceDescription={"Lorem ipsum"}
+          className={styles["orange-number"]}
         />
       </section>
     </div>
--- src/components/landingPage/services/ServiceCard.js
+++ src/components/landingPage/services/ServiceCard.js
@@ -2,27 +2,19 @@ import styles from "./ServiceCard.module.css";
 import { Link } from "react-router-dom";
 import { GoArrowUpRight } from "react-icons/go";
 
-const ServiceCard = ({
-  cardNumber,
-  serviceTitle,
-  serviceDescription,
-  className,
-}) => {
+const ServiceCard = ({ cardNumber, serviceTitle, className }) => {
   return (
     <div className={styles["card-container"]}>
       <div className={styles["card-number-container"] + " " + className}>
         <div className={styles["card-number"]}>{cardNumber}</div>
       </div>
-      <div>
+      <div className={styles["card-text-container"]}>
         <h3>{serviceTitle}</h3>
-        <p>{serviceDescription}</p>
         <Link to="/services">
-          <p>
-            Find out more
-            <span>
-              <GoArrowUpRight />
-            </span>
-          </p>
+          <div className={styles["card-link"]}>
+            <p className={styles["card-link-text"]}>Find out more</p>
+            <GoArrowUpRight className={styles["arrow-up-icon"]} />
+          </div>
         </Link>
       </div>
     </div>
