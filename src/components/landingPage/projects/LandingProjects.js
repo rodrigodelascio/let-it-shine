8e994d19fbe766bb2f4cc7b9ca644842db617aff
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-10 11:50:44 +0000

    Update background + styles

--- src/components/landingPage/projects/LandingProjects.js
+++ src/components/landingPage/projects/LandingProjects.js
@@ -1,16 +1,32 @@
 import styles from "./LandingProjects.module.css";
 import ProjectCard from "./ProjectCard";
+import spongeClean from "../../../assets/images/spongeClean.jpg";
+import handsClean from "../../../assets/images/handsCleaning.jpg";
+import prodsClean from "../../../assets/images/prodsClean.jpg";
 
 const LandingProjects = () => {
   return (
     <div className={styles["projects-wrapper"]}>
       <div className={styles["projects-header-container"]}>
-        <h1>We have a variety of projects you might be interested in</h1>
+        <h1>
+          You might be interested in <br />
+          <span>a variety of our projects</span>
+        </h1>
+        <div className={styles["projects-polka-dots"]}></div>
       </div>
       <div className={styles["projects-cards-container"]}>
-        <ProjectCard projectTitle={"Lorem ipsum dolor sit amet consectetur"} />
-        <ProjectCard projectTitle={"Lorem ipsum dolor sit amet consectetur"} />
-        <ProjectCard projectTitle={"Lorem ipsum dolor sit amet consectetur"} />
+        <ProjectCard
+          projectTitle={"Lorem ipsum dolor sit amet consectetur"}
+          cardImage={<img src={prodsClean} alt="cleaning products" />}
+        />
+        <ProjectCard
+          projectTitle={"Lorem ipsum dolor sit amet consectetur"}
+          cardImage={<img src={handsClean} alt="hands cleaning a desk" />}
+        />
+        <ProjectCard
+          projectTitle={"Lorem ipsum dolor sit amet consectetur"}
+          cardImage={<img src={spongeClean} alt="sponge cleaning surface" />}
+        />
       </div>
     </div>
   );
