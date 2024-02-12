af5a6f249c6d5002f3b2635509cc43859df0687c
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-12 14:22:03 +0000

    Add socials

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,6 +1,11 @@
 import styles from "./Footer.module.css";
 import logo from "../../assets/images/Logo_Let_It_Shine.png";
 import { Link } from "react-router-dom";
+import {
+  BiLogoInstagramAlt,
+  BiLogoFacebook,
+  BiLogoYoutube,
+} from "react-icons/bi";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -11,6 +16,17 @@ const Footer = () => {
         <Link to="/">
           <img src={logo} alt="logo" className={styles["footer-logo"]} />
         </Link>
+        <div className={styles["hero-socials"]}>
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+            <BiLogoInstagramAlt className={styles["social-icon"]} />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <BiLogoFacebook className={styles["social-icon"]} />
+          </a>
+          <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+            <BiLogoYoutube className={styles["social-icon"]} />
+          </a>
+        </div>
         <p className={styles["footer-text"]}>
           © {currentYear} Let It Shine Services Ltd
         </p>
