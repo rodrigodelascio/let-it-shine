22d2511c5a341ce4e76b5139c0fc601b43627f48
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-12 17:17:02 +0000

    Update contact page img + form

--- src/components/contactPage/hero/ContactHero.js
+++ src/components/contactPage/hero/ContactHero.js
@@ -1,10 +1,35 @@
 import styles from "./ContactHero.module.css";
+import cleaningBucket from "../../../assets/images/cleaningBucket.png";
 const ContactHero = () => {
   return (
     <div className={styles["hero-container"]}>
       <div className={styles["contact-text-wrapper"]}>
-        <h1>Get In Touch</h1>
+        <h1>We are here for you</h1>
         <h2>Let us know how we can help and we'll get back to you ASAP</h2>
+        <div className={styles["hero-image-wrapper"]}>
+          <img
+            src={cleaningBucket}
+            alt="cleaning bucket"
+            className={styles["hero-image"]}
+          />
+        </div>
+      </div>
+      <div className={styles["form-wrapper"]}>
+        <form>
+          <label>Name</label>
+          <input type="text" name="user_name" required />
+          <label>Email</label>
+          <input type="email" name="user_email" required />
+          <label>Message</label>
+          <textarea name="message" required />
+          <div className={styles["form-submit-button-wrapper"]}>
+            <input
+              type="submit"
+              value="Send"
+              className={styles["form-submit-button"]}
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
