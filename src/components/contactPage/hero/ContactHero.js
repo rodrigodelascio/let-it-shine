910a1298a3cc9b2f141e9029411e17283bd5ccdf
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-14 18:42:21 +0000

    Update info

--- src/components/contactPage/hero/ContactHero.js
+++ src/components/contactPage/hero/ContactHero.js
@@ -14,22 +14,39 @@ const ContactHero = () => {
           />
         </div> */}
       </div>
-      <div className={styles["form-wrapper"]}>
-        <form>
-          <label>Name</label>
-          <input type="text" name="user_name" required />
-          <label>Email</label>
-          <input type="email" name="user_email" required />
-          <label>Message</label>
-          <textarea name="message" required />
-          <div className={styles["form-submit-button-wrapper"]}>
-            <input
-              type="submit"
-              value="Send"
-              className={styles["form-submit-button"]}
-            />
-          </div>
-        </form>
+      <div className={styles["lower-section-container"]}>
+        <div className={styles["form-wrapper"]}>
+          <form>
+            <label>Name</label>
+            <input type="text" name="user_name" required />
+            <label>Email</label>
+            <input type="email" name="user_email" required />
+            <label>Message</label>
+            <textarea name="message" required />
+            <div className={styles["form-submit-button-wrapper"]}>
+              <input
+                type="submit"
+                value="Send"
+                className={styles["form-submit-button"]}
+              />
+            </div>
+          </form>
+        </div>
+        <div className={styles["contact-details-wrapper"]}>
+          <h3>Get In Touch</h3>
+          <p>
+            Don't hesitate to contact us, we'd love to hear from you. Ask us
+            anything, don't be shy. We are here for you, and to make your space
+            clean. Let It Shine!
+          </p>
+          <h3>Our Location</h3>
+          <p>
+            We don't have a physical office, but our main base is in
+            Walton-On-Thames.
+          </p>
+          <h3>Contact Details</h3>
+          <p>Email: 9D6zH@example.com</p>
+        </div>
       </div>
     </div>
   );
