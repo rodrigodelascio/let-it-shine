fed22d83316c5eeee7ac8b4777df414886f9a74e
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-22 16:57:07 +0000

    Create mission section

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,6 +2,7 @@ import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
 import AboutHero from "../components/aboutPage/hero/AboutHero";
 import AboutText from "../components/aboutPage/aboutText/AboutText";
+import MissionText from "../components/aboutPage/missionText/MissionText";
 
 const AboutPage = () => {
   return (
@@ -9,6 +10,7 @@ const AboutPage = () => {
       <Navbar />
       <AboutHero />
       <AboutText />
+      <MissionText />
       <Footer />
     </div>
   );
