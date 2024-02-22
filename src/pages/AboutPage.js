d8fc14b42ce9ce4d766371240a69ad5db060405c
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-22 13:02:33 +0000

    Update text + styles

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,12 +1,14 @@
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
 import AboutHero from "../components/aboutPage/hero/AboutHero";
+import AboutText from "../components/aboutPage/aboutText/AboutText";
 
 const AboutPage = () => {
   return (
     <div>
       <Navbar />
       <AboutHero />
+      <AboutText />
       <Footer />
     </div>
   );
