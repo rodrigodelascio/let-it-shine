fedb9429b8853725d421a2b2613a4d452213c1ee
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-23 14:59:26 +0000

    update styles + html tags

--- src/components/aboutPage/aboutText/AboutText.js
+++ src/components/aboutPage/aboutText/AboutText.js
@@ -3,42 +3,49 @@ import { BsDashLg } from "react-icons/bs";
 
 const AboutText = () => {
   return (
-    <div className={styles["about-section-wrapper"]}>
-      <div className={styles["about-header-container"]}>
-        <BsDashLg className={styles["dash-icon"]} />
-        <h2 className={styles["header-title"]}>Our Story</h2>
-      </div>
-      <h3 className={styles["header-subtitle"]}>
-        Let It Shine: Bringing Brilliance to <span>Homes and Businesses</span>
-      </h3>
+    <div className={styles["about-section-container"]}>
       <div className={styles["about-text-container"]}>
-        <div className={styles["about-text"]}>
-          <p>
-            At Let It Shine, we're passionate about transforming spaces into
-            havens of sparkling cleanliness and comfort. We're a local,
-            Walton-on-Thames based cleaning service dedicated to exceeding
-            expectations for both residential and commercial properties.
-          </p>
-          <p>
-            Let It Shine was born from a desire to provide unparalleled cleaning
-            services with a personal touch. We believe that clean spaces
-            shouldn't be a luxury, but a standard for everyone to enjoy. That's
-            why we offer a range of customized cleaning solutions tailored to
-            your specific needs and budget.
-          </p>
-          <p>
-            Whether you're a busy homeowner looking for a break from the
-            housework, a landlord needing a deep clean for a new tenant, or a
-            business owner seeking a reliable cleaning partner, we're here to
-            help.
-          </p>
-          <p>
-            When you choose Let It Shine, you're choosing more than just a
-            cleaning service. You're choosing peace of mind, knowing that your
-            home or business is in the hands of professionals who care.
-          </p>
+        <div className={styles["about-header-container"]}>
+          <BsDashLg className={styles["dash-icon"]} />
+          <h2 className={styles["header-title"]}>Our Story</h2>
         </div>
-        <div className={styles["about-image-container"]}></div>
+        <h3 className={styles["header-subtitle"]}>
+          Let It Shine: Bringing Brilliance to <span>Homes and Businesses</span>
+        </h3>
+        <div>
+          <div className={styles["about-text"]}>
+            <p>
+              At Let It Shine, we're passionate about transforming spaces into
+              havens of sparkling cleanliness and comfort. We're a local,
+              Walton-on-Thames based cleaning service dedicated to exceeding
+              expectations for both residential and commercial properties.
+            </p>
+            <p>
+              Let It Shine was born from a desire to provide unparalleled
+              cleaning services with a personal touch. We believe that clean
+              spaces shouldn't be a luxury, but a standard for everyone to
+              enjoy. That's why we offer a range of customized cleaning
+              solutions tailored to your specific needs and budget.
+            </p>
+            <p>
+              Whether you're a busy homeowner looking for a break from the
+              housework, a landlord needing a deep clean for a new tenant, or a
+              business owner seeking a reliable cleaning partner, we're here to
+              help.
+            </p>
+            <p>
+              When you choose Let It Shine, you're choosing more than just a
+              cleaning service. You're choosing peace of mind, knowing that your
+              home or business is in the hands of professionals who care.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles["about-image-container"]}>
+        <iframe
+          src="https://lottie.host/embed/9626666f-4f5a-4039-83fd-65a638f71cbe/uskP7xaS4J.json"
+          className={styles["about-animation"]}
+        ></iframe>
       </div>
     </div>
   );
--- src/components/aboutPage/missionText/MissionText.js
+++ src/components/aboutPage/missionText/MissionText.js
@@ -3,37 +3,45 @@ import { BsDashLg } from "react-icons/bs";
 
 const MissionText = () => {
   return (
-    <div className={styles["mission-section-wrapper"]}>
-      <div className={styles["mission-header-container"]}>
-        <BsDashLg className={styles["dash-icon"]} />
-        <h2 className={styles["header-title"]}>Our Mission</h2>
+    <div className={styles["mission-section-container"]}>
+      <div className={styles["mission-image-container"]}>
+        <iframe
+          src="https://lottie.host/embed/1bcc25e7-ca15-4395-b2d9-624946bb2e1c/IknKFVa3nE.json"
+          className={styles["mission-animation"]}
+        ></iframe>
       </div>
-      <h3 className={styles["header-subtitle"]}>
-        We Aim to Illuminate <span>Every Space</span>, Elevate
-        <span> Every Experience</span>
-      </h3>
       <div className={styles["mission-text-container"]}>
-        <div className={styles["mission-text"]}>
-          <p>
-            At Let It Shine, our mission is simple yet profound: to transform
-            spaces into beacons of sparkling cleanliness and serene comfort,
-            empowering individuals and businesses to thrive.
-          </p>
-          <p>
-            We believe that a clean environment fosters not just a visually
-            pleasing aesthetic, but also a sense of well-being, productivity,
-            and peace of mind. We strive to illuminate every corner, not just
-            with spotless surfaces, but with the confidence and comfort that
-            comes from knowing your space is cared for.
-          </p>
+        <div className={styles["mission-header-container"]}>
+          <BsDashLg className={styles["dash-icon"]} />
+          <h2 className={styles["header-title"]}>Our Mission</h2>
+        </div>
+        <h3 className={styles["header-subtitle"]}>
+          We Aim to Illuminate <span>Every Space</span>, Elevate
+          <span> Every Experience</span>
+        </h3>
+        <div>
+          <div className={styles["mission-text"]}>
+            <p>
+              At Let It Shine, our mission is simple yet profound: to transform
+              spaces into beacons of sparkling cleanliness and serene comfort,
+              empowering individuals and businesses to thrive.
+            </p>
+            <p>
+              We believe that a clean environment fosters not just a visually
+              pleasing aesthetic, but also a sense of well-being, productivity,
+              and peace of mind. We strive to illuminate every corner, not just
+              with spotless surfaces, but with the confidence and comfort that
+              comes from knowing your space is cared for.
+            </p>
 
-          <p>
-            Let It Shine isn't just about cleaning surfaces; it's about
-            illuminating the potential within every space, enabling individuals
-            and businesses to shine brighter. Join us on this mission of
-            cleanliness and well-being. Contact Let It Shine today and let us
-            help your space and your life truly shine.
-          </p>
+            <p>
+              Let It Shine isn't just about cleaning surfaces; it's about
+              illuminating the potential within every space, enabling
+              individuals and businesses to shine brighter. Join us on this
+              mission of cleanliness and well-being. Contact Let It Shine today
+              and let us help your space and your life truly shine.
+            </p>
+          </div>
         </div>
       </div>
     </div>
