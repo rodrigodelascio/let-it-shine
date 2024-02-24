7c59dda749e99e5ad909d219c206e47620a5cdc6
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-24 17:37:21 +0000

    create cards section

---
+++ src/components/aboutPage/cardsSection/AboutCards.js
@@ -0,0 +1,10 @@
+import styles from "./AboutCards.module.css";
+const AboutCards = () => {
+  return (
+    <div className={styles["about-cards-background"]}>
+      <div>AboutCards</div>
+    </div>
+  );
+};
+
+export default AboutCards;
