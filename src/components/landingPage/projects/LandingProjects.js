6fce2e616882516e3d66a628e0b265769065983a
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-02-29 12:01:09 +0000

    rewriting and styling section

--- src/components/landingPage/projects/LandingProjects.js
+++ src/components/landingPage/projects/LandingProjects.js
@@ -1,31 +1,28 @@
 import styles from "./LandingProjects.module.css";
-import ProjectCard from "./ProjectCard";
-import spongeClean from "../../../assets/images/spongeClean.jpg";
-import handsClean from "../../../assets/images/handsCleaning.jpg";
-import prodsClean from "../../../assets/images/prodsClean.jpg";
 
 const LandingProjects = () => {
   return (
-    <div className={styles["projects-wrapper"]}>
-      <div className={styles["projects-header-container"]}>
-        <h1>
-          You might be interested in <br />
-          <span>a variety of our projects</span>
-        </h1>
-      </div>
-      <div className={styles["projects-cards-container"]}>
-        <ProjectCard
-          projectTitle={"Lorem ipsum dolor sit amet consectetur"}
-          cardImage={<img src={prodsClean} alt="cleaning products" />}
-        />
-        <ProjectCard
-          projectTitle={"Mauris viverra quam sit amet eleifend"}
-          cardImage={<img src={handsClean} alt="hands cleaning a desk" />}
-        />
-        <ProjectCard
-          projectTitle={"Nullam vitae libero vel massa blandit"}
-          cardImage={<img src={spongeClean} alt="sponge cleaning surface" />}
-        />
+    <div className={styles["projects-container"]}>
+      <div className={styles["projects-wrapper"]}>
+        <div className={styles["projects-header-container"]}>
+          <h2>
+            You might be interested
+            <br />
+            <span>in our projects</span>
+          </h2>
+        </div>
+        <div className={styles["projects-description-container"]}>
+          <div className={styles["projects-description-wrapper"]}>
+            <h3>Brushes Across Borders: Connecting Global Kids</h3>
+            <p>
+              Our project was born from a desire to foster empathy and kindness
+              among children from diverse backgrounds across the globe. We also
+              aim to inspire adults to contemplate the future, considering the
+              varied realities children face around the world.
+            </p>
+          </div>
+          <div className={styles["projects-image-wrapper"]}></div>
+        </div>
       </div>
     </div>
   );
