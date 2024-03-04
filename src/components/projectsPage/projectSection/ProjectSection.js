4dcae6ee25240b944cda531e1393c73d254fdb02
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-03-04 11:10:16 +0000

    create project section + initial styles

---
+++ src/components/projectsPage/projectSection/ProjectSection.js
@@ -0,0 +1,14 @@
+import styles from "./ProjectSection.module.css";
+const ProjectSection = () => {
+  return (
+    <div>
+      <div className={styles["section-background-container"]}>
+        <div className={styles["project-section-container"]}>
+          Project Section
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSection;
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,12 +1,14 @@
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
 import ProjectsHero from "../components/projectsPage/hero/ProjectsHero";
+import ProjectSection from "../components/projectsPage/projectSection/ProjectSection";
 
 const ProjectsPage = () => {
   return (
     <div>
       <Navbar />
       <ProjectsHero />
+      <ProjectSection />
       <Footer />
     </div>
   );
