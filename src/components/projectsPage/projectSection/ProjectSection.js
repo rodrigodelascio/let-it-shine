3979ab215b3c2820a360ed5be134ad9ec53dc4e5
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-03-07 17:41:30 +0000

    add swiper slider

--- src/components/projectsPage/projectSection/ProjectSection.js
+++ src/components/projectsPage/projectSection/ProjectSection.js
@@ -1,5 +1,10 @@
 import styles from "./ProjectSection.module.css";
 import { BsDashLg } from "react-icons/bs";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 const ProjectSection = () => {
   return (
     <div>
@@ -43,8 +48,36 @@ const ProjectSection = () => {
               </div>
             </div>
           </div>
+
+          <div className={styles["project-image-container"]}>
+            <div>
+              <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                  delay: 3000,
+                  disableOnInteraction: false,
+                }}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+              >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
         </div>
-        <div className={styles["project-image-container"]}></div>
       </div>
     </div>
   );
